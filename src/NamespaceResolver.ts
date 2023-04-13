fbced2746e7ab2b7977e1bfaf2c15892f448cb70
Hothaifa Jaber <i74ifa@icloud.com>
2023-04-13 18:37:04 +0300

    format only

--- src/NamespaceResolver.ts
+++ src/NamespaceResolver.ts
@@ -1,106 +1,106 @@
-import * as vscode from 'vscode'
-import * as path from 'path'
-import { statSync } from 'fs'
+import * as vscode from 'vscode';
+import * as path from 'path';
+import { statSync } from 'fs';
 
 interface Prs4Entries {
-    prefix: string
-    path: string
+    prefix: string;
+    path: string;
 }
 
 export default class NamespaceResolver {
-    readonly msgCouldNotBeRead = "The composer.json file could not be read"
-    readonly msgNamespaceNotResolved = "The namespace could not be resolved"
-    readonly msgCouldNotBeFound = "The composer.json file could not be found"
+    readonly msgCouldNotBeRead = 'The composer.json file could not be read';
+    readonly msgNamespaceNotResolved = 'The namespace could not be resolved';
+    readonly msgCouldNotBeFound = 'The composer.json file could not be found';
 
     public async resolve(folder: string): Promise<string | undefined> {
-        let composerFilePath = this.findComposerFile(folder)
+        let composerFilePath = this.findComposerFile(folder);
         if (!composerFilePath) {
-            vscode.window.showErrorMessage(this.msgCouldNotBeFound)
-            return undefined
+            vscode.window.showErrorMessage(this.msgCouldNotBeFound);
+            return undefined;
         }
 
-        let composer = await this.composerContent(composerFilePath)
+        let composer = await this.composerContent(composerFilePath);
         if (!composer) {
-            vscode.window.showErrorMessage(this.msgCouldNotBeRead)
-            return undefined
+            vscode.window.showErrorMessage(this.msgCouldNotBeRead);
+            return undefined;
         }
 
-        const psr4Entries: Prs4Entries[] = this.collectPsr4Entries(composer, "psr-4")
-        const psr4namesp: string = this.NameSparePathPsr0(folder, composerFilePath.slice(0, -13), psr4Entries, 4)
+        const psr4Entries: Prs4Entries[] = this.collectPsr4Entries(composer, 'psr-4');
+        const psr4namesp: string = this.NameSparePathPsr0(folder, composerFilePath.slice(0, -13), psr4Entries, 4);
 
-        if (psr4namesp == "") {
-            const psr0Entries: Prs4Entries[] = this.collectPsr4Entries(composer, "psr-0")
-            return this.NameSparePathPsr0(folder, composerFilePath.slice(0, -13), psr0Entries, 0)
+        if (psr4namesp == '') {
+            const psr0Entries: Prs4Entries[] = this.collectPsr4Entries(composer, 'psr-0');
+            return this.NameSparePathPsr0(folder, composerFilePath.slice(0, -13), psr0Entries, 0);
         }
 
-        return psr4namesp
+        return psr4namesp;
     }
 
     private async composerContent(composerFilePath: string) {
         try {
-            let composerContent: string = (await vscode.workspace.openTextDocument(composerFilePath)).getText()
-            return JSON.parse(composerContent)
+            let composerContent: string = (await vscode.workspace.openTextDocument(composerFilePath)).getText();
+            return JSON.parse(composerContent);
         } catch (error) {
-            return undefined
+            return undefined;
         }
     }
 
     private findComposerFile(folder: string): string | undefined {
-        let workspaceFolder = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(folder))?.uri.fsPath
-        let segments = folder.split(path.sep)
+        let workspaceFolder = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(folder))?.uri.fsPath;
+        let segments = folder.split(path.sep);
 
-        let walking = true
+        let walking = true;
 
         do {
-            const fullPath = segments.join(path.sep)
+            const fullPath = segments.join(path.sep);
 
             try {
-                const composerPath = path.join(fullPath, 'composer.json')
-                statSync(composerPath)
+                const composerPath = path.join(fullPath, 'composer.json');
+                statSync(composerPath);
 
-                return composerPath
+                return composerPath;
             } catch {
-                segments.pop()
+                segments.pop();
             }
 
             if (fullPath == workspaceFolder) {
-                walking = false
+                walking = false;
             }
-        } while (walking)
+        } while (walking);
 
-        return undefined
+        return undefined;
     }
 
     private collectPsr4Entries(composer: any, psr4or0: string): Prs4Entries[] {
-        let autoloadEntries: { [key: string]: string } = {}
+        let autoloadEntries: { [key: string]: string } = {};
 
-        if (composer.hasOwnProperty("autoload") && composer.autoload.hasOwnProperty(psr4or0)) {
-            autoloadEntries = composer.autoload[psr4or0]
+        if (composer.hasOwnProperty('autoload') && composer.autoload.hasOwnProperty(psr4or0)) {
+            autoloadEntries = composer.autoload[psr4or0];
         }
 
-        let autoloadDevEntries: { [key: string]: string } = {}
-        if (composer.hasOwnProperty("autoload-dev") && composer["autoload-dev"].hasOwnProperty(psr4or0)) {
-            autoloadDevEntries = composer["autoload-dev"][psr4or0]
+        let autoloadDevEntries: { [key: string]: string } = {};
+        if (composer.hasOwnProperty('autoload-dev') && composer['autoload-dev'].hasOwnProperty(psr4or0)) {
+            autoloadDevEntries = composer['autoload-dev'][psr4or0];
         }
 
-        let psr4Entries: Prs4Entries[] = []
+        let psr4Entries: Prs4Entries[] = [];
 
-        this.pushTolist(psr4Entries, autoloadEntries)
-        this.pushTolist(psr4Entries, autoloadDevEntries)
+        this.pushTolist(psr4Entries, autoloadEntries);
+        this.pushTolist(psr4Entries, autoloadDevEntries);
 
-        return psr4Entries
+        return psr4Entries;
     }
 
-    private pushTolist(psr4Entries:Prs4Entries[], entries:{[key:string]: string}) {
+    private pushTolist(psr4Entries: Prs4Entries[], entries: { [key: string]: string }) {
         for (const prefix in entries) {
-            const entryPath = entries[prefix]
+            const entryPath = entries[prefix];
 
             if (Array.isArray(entryPath)) {
                 for (const prefixPath of entryPath) {
-                    psr4Entries.push({ prefix: prefix, path: prefixPath })
+                    psr4Entries.push({ prefix: prefix, path: prefixPath });
                 }
             } else {
-                psr4Entries.push({ prefix: prefix, path: entryPath })
+                psr4Entries.push({ prefix: prefix, path: entryPath });
             }
         }
     }
@@ -108,69 +108,68 @@ export default class NamespaceResolver {
     private ootrim(str: string, char: string, type?: string): string {
         if (char) {
             if (type == 'left') {
-                return str.replace(new RegExp('^\\' + char + '+', 'g'), '')
+                return str.replace(new RegExp('^\\' + char + '+', 'g'), '');
             } else if (type == 'right') {
-                return str.replace(new RegExp('\\' + char + '+$', 'g'), '')
+                return str.replace(new RegExp('\\' + char + '+$', 'g'), '');
             }
 
-            return str.replace(new RegExp('^\\' + char + '+|\\' + char + '+$', 'g'), '')
+            return str.replace(new RegExp('^\\' + char + '+|\\' + char + '+$', 'g'), '');
         }
 
-        return str.replace(/^\s+|\s+$/g, '')
+        return str.replace(/^\s+|\s+$/g, '');
     }
 
-    private NameSparePathPsr0(filePath: string, composerdir: string, Prs0ent: Prs4Entries[], prs:number): string {
-        let enti: Prs4Entries = { path: "", prefix: "" }
-        let current_dir:string = ""
-        let srcIndex:number    = -1
+    private NameSparePathPsr0(filePath: string, composerdir: string, Prs0ent: Prs4Entries[], prs: number): string {
+        let enti: Prs4Entries = { path: '', prefix: '' };
+        let current_dir: string = '';
+        let srcIndex: number = -1;
 
-        filePath    = this.ootrim(filePath, path.sep, "right") + path.sep
+        filePath = this.ootrim(filePath, path.sep, 'right') + path.sep;
 
         for (const entip in Prs0ent) {
-            enti = Prs0ent[entip]
-            enti.prefix = this.ootrim(enti.prefix.trim(), '\\').trim()
-            enti.path   = this.checkEmptyPath( this.ootrim(enti.path, '/') )
+            enti = Prs0ent[entip];
+            enti.prefix = this.ootrim(enti.prefix.trim(), '\\').trim();
+            enti.path = this.checkEmptyPath(this.ootrim(enti.path, '/'));
 
             if (enti.path.length > 0) {
-                current_dir = composerdir + enti.path + path.sep
+                current_dir = composerdir + enti.path + path.sep;
                 if (enti.prefix.length > 0 && prs == 0) {
                     current_dir += enti.prefix.split('\\').join(path.sep) + path.sep;
                 }
             } else {
-                current_dir = composerdir
+                current_dir = composerdir;
             }
 
-            srcIndex = filePath.indexOf(current_dir)
+            srcIndex = filePath.indexOf(current_dir);
 
             if (srcIndex == 0) {
-                break
+                break;
             }
         }
 
-        if(srcIndex == -1) {
-            return ""
+        if (srcIndex == -1) {
+            return '';
         }
 
-        let pathElements = this.ootrim( filePath,  path.sep, "right" ).slice(current_dir.length ).split(path.sep).join("\\").trim();
-        let slash = ""
+        let pathElements = this.ootrim(filePath, path.sep, 'right').slice(current_dir.length).split(path.sep).join('\\').trim();
+        let slash = '';
 
         if (enti.prefix.length > 0 && pathElements.length > 0) {
-            slash = "\\"
+            slash = '\\';
         }
 
-        return enti.prefix + slash + pathElements
+        return enti.prefix + slash + pathElements;
     }
 
-    private checkEmptyPath(path:string) {
-        path = this.ootrim( path.trim(), "./", 'left')
-        switch(path) {
+    private checkEmptyPath(path: string) {
+        path = this.ootrim(path.trim(), './', 'left');
+        switch (path) {
             case '.':
             case '/':
             case './':
-                return ""
+                return '';
             default:
-                return path
+                return path;
         }
     }
-
 }
